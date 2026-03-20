ce588932960ec25d830af0950942364e91a1d6f3
paulinhacruz-design <paulinha_cruz@estudante.sesisenai.org.br>
2026-03-20 19:12:04 +0000

    update

---
+++ programa5.py
@@ -0,0 +1,5 @@
+largura = (input("digite a altura do terreno em metros: "))
+altura = float(input("digite a altura do terro em metros: "))
+
+area = altura * largura
+print("area do meu terreno é: ". area)
---
+++ programa8.py
@@ -0,0 +1,6 @@
+nome_pessoa = input("insira seu nome")
+idade_pessoas = int(input("insira sua idade"))
+
+dias_vida = idade_pessoas * 365 
+
+print(" seu total de dias é: ",dias_vida)
